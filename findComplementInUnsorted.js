1fa34cb4f4a3845ea3b63486d2058732791bcc1f
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-04 12:09:32 -0700

    Solve the solution with the second way

--- findComplementInUnsorted.js
+++ findComplementInUnsorted.js
@@ -4,19 +4,13 @@ var testData  = [64, 22, 8, 12, 27, 13, 21, 40, 47, 95, 3];
 var targetNum = 30;
 var findPairs = function(arr, target) {
       var qualifiedPairs = [];
-      var leftPointer = 0, rightPointer = arr.length - 1;
+      var checkedNumbers = {};
 
-      while (leftPointer < rightPointer){
-        var pairSum = arr[leftPointer] + arr[rightPointer];
-
-        if (pairSum === target) {
-          qualifiedPairs.push([arr[leftPointer], arr[rightPointer]]);
-          leftPointer++;
-          rightPointer--;
-        }
-        else if (pairSum < target) {leftPointer++;}
-        else {rightPointer--;}
-      }
+      arr.forEach(function(number){
+        var complement = target - number;
+        if (checkedNumbers[complement]){qualifiedPairs.push([number, complement]);}
+        else {checkedNumbers[number] = true;}
+      });
 
       return qualifiedPairs;
     }
