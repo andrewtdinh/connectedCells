af66079037a3f88954feee7a747f1465b80bf1a4
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-04 10:28:52 -0700

    Refactor function again

--- findComplementInSortedArray.js
+++ findComplementInSortedArray.js
@@ -4,11 +4,11 @@ var testData  = [3, 8, 12, 13, 21, 22, 27, 40, 47, 64, 95];
 var targetNum = 30;
 var findPairs = function(arr, target) {
       var qualifiedPairs = [];
-      var leftPointer = 0;
-      var rightPointer = arr.length - 1;
+      var leftPointer = 0, rightPointer = arr.length - 1;
 
       while (leftPointer < rightPointer){
         var pairSum = arr[leftPointer] + arr[rightPointer];
+
         if (pairSum === target) {
           qualifiedPairs.push([arr[leftPointer], arr[rightPointer]]);
           leftPointer++;
