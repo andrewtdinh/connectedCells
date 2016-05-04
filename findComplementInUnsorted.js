b30d834fcee2c6fcc2234baf6b0d5cb6523bae52
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-04 11:57:35 -0700

    Just copied over from the other function and fix later

---
+++ findComplementInUnsorted.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var testData  = [3, 8, 12, 13, 21, 22, 27, 40, 47, 64, 95];
+var targetNum = 30;
+var findPairs = function(arr, target) {
+      var qualifiedPairs = [];
+      var leftPointer = 0, rightPointer = arr.length - 1;
+
+      while (leftPointer < rightPointer){
+        var pairSum = arr[leftPointer] + arr[rightPointer];
+
+        if (pairSum === target) {
+          qualifiedPairs.push([arr[leftPointer], arr[rightPointer]]);
+          leftPointer++;
+          rightPointer--;
+        }
+        else if (pairSum < target) {leftPointer++;}
+        else {rightPointer--;}
+      }
+
+      return qualifiedPairs;
+    }
+
+console.log(findPairs(testData, targetNum));
