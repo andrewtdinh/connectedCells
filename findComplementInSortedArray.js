5d45d0369c6e1eaac843562d91813bf2a5a3ab05
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-04 10:24:58 -0700

    Fix bug, 3rd try

--- findComplementInSortedArray.js
+++ findComplementInSortedArray.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var testData  = [3, 8, 12, 13, 21, 22, 27, 40, 47, 64, 95],
+var testData  = [3, 8, 12, 13, 21, 22, 27, 40, 47, 64, 95];
 var targetNum = 30;
 var findPairs = function(arr, target) {
       var qualifiedPairs = [];
@@ -10,7 +10,11 @@ var findPairs = function(arr, target) {
 
       while (leftPointer < rightPointer){
         var pairSum = arr[leftPointer] + arr[rightPointer];
-        if (pairSum === target) {qualifiedPairs.push([arr[leftPointer], arr[rightPointer]]);}
+        if (pairSum === target) {
+          qualifiedPairs.push([arr[leftPointer], arr[rightPointer]]);
+          leftPointer++;
+          rightPointer--;
+        }
         else if (pairSum < target) {leftPointer++;}
         else {rightPointer--;}
       }
