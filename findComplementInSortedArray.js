dd5f8e52d035770813caa775a9642649f6e824bc
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-04 10:09:21 -0700

    Incomplete file added

---
+++ findComplementInSortedArray.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var testData  = [3, 8, 12, 13, 21, 22, 27, 40, 47, 64, 95],
+    targetNum = 30,
+    findPairs = function(arr, target) {
+      var arrLen = arr.length;
+      var leftPointer = 0;
+      var rightPointer = arrLen - 1;
+    }
