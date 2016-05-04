7caf83039ca7b95c3b7790c37dcfae9ffcc49056
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-04 10:19:11 -0700

    First try for findComplementInSortedArray

--- findComplementInSortedArray.js
+++ findComplementInSortedArray.js
@@ -3,7 +3,19 @@
 var testData  = [3, 8, 12, 13, 21, 22, 27, 40, 47, 64, 95],
     targetNum = 30,
     findPairs = function(arr, target) {
+      var qualifiedPairs = [];
       var arrLen = arr.length;
       var leftPointer = 0;
       var rightPointer = arrLen - 1;
+
+      while (leftPointer < rightPointer){
+        var pairSum = arr[leftPointer] + arr[rightPointer];
+        if (pairSum === target) {qualifiedPairs.push([arr[leftPointer], arr[rightPointer]]);}
+        else if (pairSum < target) {leftPointer++;}
+        else {rightPointer--;}
+      }
+
+      return qualifiedPairs;
     }
+
+console.log(findPairs(testData, targetNum));
