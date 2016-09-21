eac87a7d84942278744ebd3c518f24bb89854fdc
Andrew Dinh <andrewdinh74@yahoo.com>
2016-09-21 16:43:18 -0700

    Define method to check if an integer is a palindrome by converting integer to string and call the string palindrome check

---
+++ checkPalindromeInt.py
@@ -0,0 +1,8 @@
+import checkPalindromeStr
+
+# The functions below will check if an integer is a Palindrome:
+print isPalindrome('abbA')
+
+
+def strPalindromeCheck(num):
+    return isPalindrome(str(num))
