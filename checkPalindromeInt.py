fa2956ec6c3db0787e6f6bc8a1b3e6debc893337
Andrew Dinh <andrewdinh74@yahoo.com>
2016-09-21 17:12:02 -0700

    Do a check for empty strings first

--- checkPalindromeInt.py
+++ checkPalindromeInt.py
@@ -1,8 +1,9 @@
-import checkPalindromeStr
+from checkPalindromeStr import isPalindrome
 
-# The functions below will check if an integer is a Palindrome:
-print isPalindrome('abbA')
-
+# The functions below will check if an integer is a Palindrome by converting first to string:
 
 def strPalindromeCheck(num):
     return isPalindrome(str(num))
+
+# print strPalindromeCheck(24)
+# print strPalindromeCheck(242)
--- checkPalindromeStr.py
+++ checkPalindromeStr.py
@@ -1,9 +1,12 @@
 # This will test if a string is a palindrome
 
 def isPalindrome(aStr):
+    # If the string is empty
+    if not aStr:
+        return False
     aStr = aStr.lower()
     return aStr[::-1] == aStr
 
 
-print isPalindrome('abc')
-print isPalindrome('abccbA')
+# print isPalindrome('abc')
+# print isPalindrome('abccbA')
