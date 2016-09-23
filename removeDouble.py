f0d9425b42824310e38af1e8ecbe0ae1ea8fda24
Andrew Dinh <andrewdinh74@yahoo.com>
2016-09-23 10:23:02 -0700

    Rewriting function

--- removeDouble.py
+++ removeDouble.py
@@ -11,19 +11,16 @@ def removeDoubles(s):
         else:
             oldString = newString
         searchLimit = len(newString) - 1
-        for ch in newString:
-            indx = newString.index(ch)      #This is where a bug is introduced
+        for indx in xrange(searchLimit):
+            # indx = newString.index(ch)      #This is where a bug is introduced
             print 'index: ' + str(indx)
-            if indx == searchLimit:
-                break
-            else:
-                if ch == newString[indx + 1]:
-                    if indx + 2 > searchLimit:
-                        newString = newString[0:indx]
-                        break
-                    else:
-                        newString = newString[0:indx] + newString[(indx + 2):]
-                        break
+            if newString[indx] == newString[indx + 1]:
+                if indx + 2 > searchLimit:
+                    newString = newString[0:indx]
+                    break
+                else:
+                    newString = newString[0:indx] + newString[(indx + 2):]
+                    break
         print newString
         if newString == oldString:
             break
