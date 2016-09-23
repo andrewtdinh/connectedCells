4dd39e5e2ffa0f1f682097d83749531a53e40ba0
Andrew Dinh <andrewdinh74@yahoo.com>
2016-09-22 19:40:22 -0700

    WIP

--- removeDouble.py
+++ removeDouble.py
@@ -7,7 +7,18 @@ def removeDoubles(s):
         oldString = newString
         searchLimit = len(oldString) - 1
         for ch in oldString:
-            if oldString.index(ch) == searchLimit:
+            indx = oldString.index(ch)
+            if indx == searchLimit:
                 break
             else:
-                
+                if ch == oldString[indx + 1]:
+                    if indx + 2 > searchLimit:
+                        newString = oldString[0:indx]
+                        break
+                    else:
+                        newString = oldString[0:indx] + oldString[(indx + 2):]
+                        break
+    return newString
+
+
+print (removeDoubles('aabcdcbffb'))
